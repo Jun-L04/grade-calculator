03faed873ce88a65169b8cb6e29cfd79269427ef
Junyang Lu <LuJunyang04@gmail.com>
2024-12-22 18:38:41 -0500

    stuff

--- miniumGradeCalc.py
+++ miniumGradeCalc.py
@@ -1,11 +1,12 @@
-
+from my_grades import *
+# not showing u my actual grades lol
 
 # key: specific assignment
 # value: list[current grade, weight]
 
 # weight of the final
-finalWeight = 0.125
-gradeWanted = 93
+finalWeight = 0.25
+gradeWanted = 90
 
 
 def finalGradeNeeded(gradesDict, finalWeight, gradeWanted):
@@ -19,4 +20,4 @@ def finalGradeNeeded(gradesDict, finalWeight, gradeWanted):
 
 
 print(
-    f'Need a final grade of: {finalGradeNeeded(gradesDict485, finalWeight, gradeWanted)}')
+    f'Need a final grade of: {finalGradeNeeded(gradesDict311, finalWeight, gradeWanted)}')
